57a763636193255da44405c8740c081197b885c2
timotej <lovrec.timotej@gmail.com>
2026-03-31 19:15:28 +0200

    fix for vsp

--- resources/js/Pages/User/Show.jsx
+++ resources/js/Pages/User/Show.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import { USER_STATUS_CLASS_MAP, USER_STATUS_TEXT_MAP } from "@/constants.jsx";
 import TasksTable from "../Task/TasksTable";
+
 export default function Show({ auth, user, tasks, queryParams }) {
     return (
         <AuthenticatedLayout
@@ -16,90 +16,32 @@ export default function Show({ auth, user, tasks, queryParams }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div>
-                            <img
-                                src={user.image_path}
-                                alt=""
-                                className="w-full h-64 object-cover"
-                            />
-                        </div>
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="grid gap-1 grid-cols-2 mt-2">
                                 <div>
-                                    <div>
-                                        <label className="font-bold text-lg">
-                                            User ID
-                                        </label>
-                                        <p className="mt-1">{user.id}</p>
-                                    </div>
-                                    <div className="mt-4">
-                                        <label className="font-bold text-lg">
-                                            User Name
-                                        </label>
-                                        <p className="mt-1">{user.name}</p>
-                                    </div>
-
-                                    <div className="mt-4">
-                                        <label className="font-bold text-lg">
-                                            User Status
-                                        </label>
-                                        <p className="mt-1">
-                                            <span
-                                                className={
-                                                    "px-2 py-1 rounded text-white " +
-                                                    USER_STATUS_CLASS_MAP[
-                                                        user.status
-                                                    ]
-                                                }
-                                            >
-                                                {
-                                                    USER_STATUS_TEXT_MAP[
-                                                        user.status
-                                                    ]
-                                                }
-                                            </span>
-                                        </p>
-                                    </div>
-                                    <div className="mt-4">
-                                        <label className="font-bold text-lg">
-                                            Created By
-                                        </label>
-                                        <p className="mt-1">
-                                            {user.createdBy.name}
-                                        </p>
-                                    </div>
+                                    <label className="font-bold text-lg">
+                                        User ID
+                                    </label>
+                                    <p className="mt-1">{user.id}</p>
                                 </div>
                                 <div>
-                                    <div>
-                                        <label className="font-bold text-lg">
-                                            Due Date
-                                        </label>
-                                        <p className="mt-1">{user.due_date}</p>
-                                    </div>
-                                    <div className="mt-4">
-                                        <label className="font-bold text-lg">
-                                            Create Date
-                                        </label>
-                                        <p className="mt-1">
-                                            {user.created_at}
-                                        </p>
-                                    </div>
-                                    <div className="mt-4">
-                                        <label className="font-bold text-lg">
-                                            Updated By
-                                        </label>
-                                        <p className="mt-1">
-                                            {user.updatedBy.name}
-                                        </p>
-                                    </div>
+                                    <label className="font-bold text-lg">
+                                        User Name
+                                    </label>
+                                    <p className="mt-1">{user.name}</p>
                                 </div>
-                            </div>
-
-                            <div className="mt-4">
-                                <label className="font-bold text-lg">
-                                    User Description
-                                </label>
-                                <p className="mt-1">{user.description}</p>
+                                <div className="mt-4">
+                                    <label className="font-bold text-lg">
+                                        Email
+                                    </label>
+                                    <p className="mt-1">{user.email}</p>
+                                </div>
+                                <div className="mt-4">
+                                    <label className="font-bold text-lg">
+                                        Created At
+                                    </label>
+                                    <p className="mt-1">{user.created_at}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
